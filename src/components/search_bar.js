5fdfc3a868d47de4c7c920f92c1c1c4ddef70fdd
Ian Duke <ian.c.duke@gmail.com>
2017-08-23 23:09:51 -0400

    Search bar and video list updates dynamically

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,20 +4,27 @@ class SearchBar extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = { term: '' };
   }
 
   render () {
     return (
-      <div>
+      <div className="search-bar">
         <input
+          placeholder="Search..."
           value={this.state.term}
-          onChange={ev => this.setState({ term: ev.target.value })}
+          onChange={ev => this.onInputChange(ev.target.value)}
         />
       </div>
     );
   }
+
+  onInputChange = (term) => {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -5,25 +5,38 @@ const API_KEY = process.env.API_KEY;
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    youtubeSearch({key: API_KEY, term: 'shiba inu' }, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('shiba inu');
+  }
+
+  videoSearch = (term) => {
+    youtubeSearch({key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
