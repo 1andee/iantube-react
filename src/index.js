6ed5249a68dde04c628e5f4708cd12eb561fb4ee
Ian Duke <ian.c.duke@gmail.com>
2017-08-23 22:03:51 -0400

    Video titles & thumbnails for hard-coded query display in window

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
+import youtubeSearch from 'youtube-api-search';
+const API_KEY = process.env.API_KEY;
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    youtubeSearch({key: API_KEY, term: 'shiba inu' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
